c8c26795ca946de6649164d7d4bd3156d5122b03
alexandreservian <alexandreservian@gmail.com>
2020-03-21 19:17:23 -0300

    Foi criado um supervisor dinamico para os processos

---
+++ lib/stack_dynamic_supervisor.ex
@@ -0,0 +1,16 @@
+defmodule DynamicSupervisorStack do
+  use DynamicSupervisor
+
+  def start_link() do
+    DynamicSupervisor.start_link __MODULE__, :ok, name: __MODULE__
+  end
+
+  def start_process(process) do
+    DynamicSupervisor.start_child __MODULE__, process
+  end
+
+  def init(:ok), do: DynamicSupervisor.init(strategy: :one_for_one)
+  def count_children(), do: DynamicSupervisor.count_children __MODULE__
+  def which_children(), do: DynamicSupervisor.which_children __MODULE__
+  def terminate_process(pid), do: DynamicSupervisor.terminate_child __MODULE__, pid
+end
