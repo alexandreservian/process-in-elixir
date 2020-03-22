422c7581f2959c33fa17a3415890b7ee98fec84c
alexandreservian <alexandreservian@gmail.com>
2020-03-22 15:39:07 -0300

    Colocando o projeto para iniciar uma arvore de supervisão

---
+++ lib/start_app.ex
@@ -0,0 +1,8 @@
+defmodule StartApp  do
+  use Application
+
+  def start(_type, _args) do
+    children = [StackSupervisor, ProcessInElixir.BookAgent]
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
